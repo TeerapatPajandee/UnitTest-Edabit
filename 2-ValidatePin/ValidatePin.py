a383374cf88dac90a6976b0a384a6b2b36602ff2
TeerapatPajandee <6106021622071@fitm.kmutnb.ac.th>
2021-04-01 11:09:57 +0700

    update ValidatePin

---
+++ 2-ValidatePin/ValidatePin.py
@@ -0,0 +1,24 @@
+# Pin = input("Enter Pin 4 or 6 Characters :")
+
+
+def valid(txt):
+    print(txt)
+
+    if txt.isnumeric() == True:
+        sum = 0
+        for x in txt:
+            sum += 1
+        # print(sum)
+        if sum == 4 or sum == 6:
+            return True
+        else:
+            return False
+    else:
+        return False
+
+
+### Test ###
+# if valid(Pin):
+#     print("OK!")
+# else:
+#     print("Invalid")
